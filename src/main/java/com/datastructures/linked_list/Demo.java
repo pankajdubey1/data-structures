e679327c9c7341b9a525c44de3c8300c4f91f9f4
Pankaj Dubey <pankaj08.india@gmail.com>
2019-04-23 14:52:09 +0530

    added linked list

--- src/main/java/com/datastructures/linked_list/Demo.java
+++ src/main/java/com/datastructures/linked_list/Demo.java
@@ -2,39 +2,40 @@ package com.datastructures.linked_list;
 
 public class Demo {
 
-    public static void main(String[] args) {
-        //LinkedList linkedList = getLinkedList();
-        LinkedList linkedList = new LinkedList();
-        Node node = new Node(4);
-        linkedList.add(1, node);
-        printLinkedList(linkedList.head);
+	public static void main(String[] args) {
 
-    }
+		LinkedList linkedList = getLinkedList();
+		linkedList.add(new Node(4));
+		System.out.print("Original List: ");
+		linkedList.printLinkedList();
+		System.out.println();
+		System.out.print("After deleteing the node at 1st position, List: ");
+		linkedList.delete(1);
+		linkedList.printLinkedList();
+		System.out.println();
+		System.out.print("After reversing, List: ");
+		linkedList.reverse();
+		linkedList.printLinkedList();
+		System.out.println();
+		System.out.print("reverse print, List: ");
+		linkedList.reversePrint(linkedList.head);
+		linkedList.reverse(linkedList.head);
+		System.out.println();
+		System.out.print("reverse, List: ");
+		linkedList.printLinkedList();
 
-    private static void addNodeAtTheBeginning(LinkedList linkedList, Node node) {
-        Node head = linkedList.head;
-        linkedList.head = node;
-        node.setNext(head);
-    }
+	}
 
-    private static void printLinkedList(Node head) {
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.getData());
-            temp = temp.getNext();
-        }
-    }
+	private static LinkedList getLinkedList() {
+		LinkedList linkedList = new LinkedList();
+		Node node1 = new Node(1);
+		linkedList.head = node1;
 
-    private static LinkedList getLinkedList() {
-        LinkedList linkedList = new LinkedList();
-        Node node1 = new Node(1);
-        linkedList.head = node1;
+		Node node2 = new Node(2);
+		node1.setNext(node2);
 
-        Node node2 = new Node(2);
-        node1.setNext(node2);
-
-        Node node3 = new Node(3);
-        node2.setNext(node3);
-        return linkedList;
-    }
+		Node node3 = new Node(3);
+		node2.setNext(node3);
+		return linkedList;
+	}
 }
--- src/main/java/com/datastructures/linked_list/LinkedList.java
+++ src/main/java/com/datastructures/linked_list/LinkedList.java
@@ -11,13 +11,73 @@ public class LinkedList {
 			addAtTheEnd(node);
 	}
 
-	public void add(int index, Node node) {
-		if (index == 1) {
+	public void add(int position, Node node) {
+		if (position == 1) {
 			Node temp = this.head;
 			this.head = node;
 			node.setNext(temp);
 		} else
-			addAtIndex(index, node);
+			addAtIndex(position, node);
+	}
+
+	public void delete(int position) {
+		Node temp = this.head;
+		if (temp == null)
+			System.out.println("List is empty");
+		else if (position == 1) {
+			this.head = temp.getNext();
+			temp = null;
+		} else
+			deleteNode(position);
+	}
+
+	public void reverse() {
+		Node current = this.head;
+		Node prev = null;
+		while (current != null) {
+			Node next = current.getNext();
+			current.setNext(prev);
+			prev = current;
+			current = next;
+		}
+		this.head = prev;
+	}
+
+	public void reversePrint(Node head) {
+		if (head == null)
+			return;
+		reversePrint(head.getNext());
+		System.out.print(head.getData() + " ");
+	}
+
+	public void reverse(Node p) {
+		if (p == null || p.getNext() == null) {
+			this.head = p;
+			return;
+		} else {
+			reverse(p.getNext());
+			Node q = p.getNext();
+			q.setNext(p);
+			p.setNext(null);
+		}
+	}
+
+	public void printLinkedList() {
+		Node temp = this.head;
+		while (temp != null) {
+			System.out.print(temp.getData() + " ");
+			temp = temp.getNext();
+		}
+	}
+
+	private void deleteNode(int position) {
+		Node temp = this.head;
+		for (int i = 1; i < position - 1; i++) {
+			temp = temp.getNext();
+		}
+		Node temp2 = temp.getNext();
+		temp.setNext(temp2.getNext());
+		temp2 = null;
 	}
 
 	private void addAtIndex(int index, Node node) {
